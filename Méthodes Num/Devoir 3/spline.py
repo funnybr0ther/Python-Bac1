ce342940e8121d064ac871a6e298c10b26b59b5d
Guillaume van der Rest <guillaumevdrest@gmail.com>
2019-03-05 11:41:23 +0100

    Devoir 3 fonctionnel

--- Méthodes Num/Devoir 3/spline.py
+++ Méthodes Num/Devoir 3/spline.py
@@ -3,23 +3,20 @@ from numpy import *
 from numpy.linalg import solve
 
 def spline(x,h,U):
-    n = size(U)
-    X = arange(0,n+1)*h
-    print(n)
-    l = zeros(len(x),dtype=int)
-    for j in range(1,n):
-        l[X[j]<=x] = j
-    matrice = []
-    matrice = diag(linspace(4,4,n),0) + diag(linspace(1,1,n-1),-1) + diag(linspace(1,1,n-1),1)
-    matrice[0][n-1] = 1
-    matrice[n-1][0] = 1
-    matriceU = diag(linspace(-2,-2,n),0) + diag(linspace(1,1,n-1),-1) + diag(linspace(1,1,n-1),1)
-    matriceU[0][n-1] = 1
-    matriceU[n-1][0] = 1
-    matriceU *= (h**2)/6
-    print(dot(matriceU,U))
-    ddU = solve(array(matrice),dot(matriceU,U))
-    u = zeros(size(x))    
-    for i in range(0,size(x)):
-        u[i] = ddU[l[i]-1]*(X[l[i]] - x[i])**3 /(6*h) + ddU[l[i]]*(x[i] - X[l[i]-1])**3 + (U[l[i]-1]/h - ddU[l[i]-1]*h/6)*(X[l[i]] -x[i]) + (U[l[i]]/h - ddU[l[i]]*h/6)*(x[i] - X[l[i]-1])
-    return u
+   n = size(U)
+   X = arange(0,n+1)*h
+   i = zeros(len(x),dtype=int)
+   for j in range(1,n):
+      i[X[j]<=x] = j
+   matrice = diag(linspace(4,4,n),0) + diag(linspace(1,1,n-1),-1) + diag(linspace(1,1,n-1),1)
+   matrice[0][n-1] = 1
+   matrice[n-1][0] = 1
+   matriceU = diag(linspace(-2,-2,n),0) + diag(linspace(1,1,n-1),-1) + diag(linspace(1,1,n-1),1)
+   matriceU[0][n-1] = 1
+   matriceU[n-1][0] = 1
+   ddU = solve(matrice*(h**2)/6,dot(matriceU,U))
+   ddU = append(ddU,ddU[0])
+   U = append(U,U[0])
+   b = X[i+1] - x
+   a = x - X[i]
+   return b * (ddU[i]/6 * ((b * b)/h - h) + U[i]/h) + a*( (ddU[i+1]/6)*((a*a)/h - h) + U[i+1]/h )
--- Méthodes Num/Devoir 3/splinetest.py
+++ Méthodes Num/Devoir 3/splinetest.py
@@ -7,7 +7,7 @@ n = 5
 h = 3*pi/(2*(n+1))
 T = arange(0,3*pi/2,h)
 X = cos(T); Y = sin(T)
- 
+
 fig = plt.figure()
 plt.plot(X,Y,'.r',markersize=10)
 t = linspace(0,2*pi,100)
